711af8e4e463809815e964570c4215033773b0a1
Dan Amador <danamadorpe@gmail.com>
2020-05-21 11:36:27 +0200

    fixed logs from other modules

--- Dataset/util/dataset_logger.py
+++ Dataset/util/dataset_logger.py
@@ -1,7 +1,12 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("psaw").setLevel(logging.WARNING)
+
 # Create handler
+dataset_logger = logging.getLogger('crawler')
+logging.getLogger("crawler").setLevel(logging.DEBUG)
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler('file.log')
 c_handler.setLevel(logging.WARNING)
@@ -12,10 +17,9 @@ c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
-dataset_logger = logging.getLogger('crawler')
 # Add handlers to the logger
 dataset_logger.addHandler(c_handler)
 dataset_logger.addHandler(f_handler)
-logging.getLogger().setLevel(logging.DEBUG)
+dataset_logger.setLevel(logging.DEBUG)
 
 
